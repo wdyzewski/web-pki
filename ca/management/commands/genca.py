a63706833cf0e8d54137f050b186ab1157cc7f73
Wojciech Dyżewski <mdyzio+github@staszic.waw.pl>
2023-03-06 23:10:08 +0100

    rewritten genca - this time using cryptography module

--- ca/management/commands/genca.py
+++ ca/management/commands/genca.py
@@ -1,10 +1,16 @@
-from django.core.management.base import BaseCommand, CommandError
-from OpenSSL import crypto, SSL
-from os.path import join
+import datetime
 import random
+
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import NameOID
+from django.core.management.base import BaseCommand, CommandError
 
 CA_CRT = 'ca.crt'
 CA_KEY = 'ca.key'
+CA_CN = 'Wojciech Dyzewski CA'
 
 class Command(BaseCommand):
     help = 'Generates new CA'
@@ -22,28 +28,38 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         key_size = options['key_size']
 
-        key = crypto.PKey()
-        key.generate_key(crypto.TYPE_RSA, key_size)
-        serialnumber = random.getrandbits(64)
-        cert = crypto.X509()
-        """
-        cert.get_subject().C = input("Country: ")
-        cert.get_subject().ST = input("State: ")
-        cert.get_subject().L = input("City: ")
-        cert.get_subject().O = input("Organization: ")
-        cert.get_subject().OU = input("Organizational Unit: ")
-        cert.get_subject().CN = CN
-        """
-        cert.set_serial_number(serialnumber)
-        cert.gmtime_adj_notBefore(0)
-        cert.gmtime_adj_notAfter(31536000) #315360000 is in seconds.
-        cert.set_issuer(cert.get_subject())
-        cert.set_pubkey(key)
-        cert.sign(key, 'sha512')
-        crt_pem = crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
-        key_pem = crypto.dump_privatekey(crypto.FILETYPE_PEM, key)
-        with open(CA_CRT,"wt") as f:
-            f.write(crt_pem.decode("utf-8"))
-        with open(CA_KEY, "wt") as f:
-            f.write(key_pem.decode("utf-8"))
+        key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=key_size
+        )
+        # Write our key to disk for safe keeping
+        with open(CA_KEY, "wb") as f:
+            f.write(key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption()
+            ))
+        builder = x509.CertificateBuilder()
+        # TODO add country, state, city... (if passed)
+        builder = builder.subject_name(x509.Name([
+            x509.NameAttribute(NameOID.COMMON_NAME, CA_CN)
+        ]))
+        builder = builder.issuer_name(x509.Name([
+            x509.NameAttribute(NameOID.COMMON_NAME, CA_CN),
+        ]))
+        builder = builder.not_valid_before(datetime.datetime.now())
+        builder = builder.not_valid_after(datetime.datetime.now() + datetime.timedelta(days=365 * 10)) # TODO --validity
+        builder = builder.serial_number(x509.random_serial_number())
+        builder = builder.public_key(key.public_key())
+        builder = builder.add_extension(
+            x509.BasicConstraints(ca=True, path_length=None), critical=True,
+        )
+        certificate = builder.sign(
+            private_key=key, algorithm=hashes.SHA256()
+        )
+        with open("ca.crt", "wb") as f:
+            f.write(certificate.public_bytes(
+                encoding=serialization.Encoding.PEM
+            ))
+
         self.stdout.write(self.style.SUCCESS('Successfully generated CA'))
