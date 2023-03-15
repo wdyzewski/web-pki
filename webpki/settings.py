326dc343f02d500e69a1b75b02e9a147581a4550
Wojciech Dyżewski <mdyzio+github@staszic.waw.pl>
2023-03-15 18:41:30 +0100

    created simple login and logout pages

--- webpki/settings.py
+++ webpki/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'webpki.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# destination page after successful login/logout
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
